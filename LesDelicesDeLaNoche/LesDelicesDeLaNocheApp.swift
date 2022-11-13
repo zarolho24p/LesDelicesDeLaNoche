b1346e190ee2adae1fa8bedd71bd81a5854acdb4
zarolho <dadinio24p@gmail.com>
2022-11-14 00:42:11 +0100

    Initial Commit

---
+++ LesDelicesDeLaNoche/LesDelicesDeLaNocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  LesDelicesDeLaNocheApp.swift
+//  LesDelicesDeLaNoche
+//
+//  Created by mafiosa on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LesDelicesDeLaNocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
